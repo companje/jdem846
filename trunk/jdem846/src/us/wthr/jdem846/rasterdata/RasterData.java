7a5782b47b057cf2de2fc9c6bcb4a4aa594c9b3f
kmsmgill@gmail.com <kmsmgill@gmail.com@966e72df-b6db-f143-e4ac-e5bb048938f7>
2011-10-25 13:39:27 +0000

    * Starting new raster data system

---
+++ trunk/jdem846/src/us/wthr/jdem846/rasterdata/RasterData.java
@@ -0,0 +1,32 @@
+package us.wthr.jdem846.rasterdata;
+
+import us.wthr.jdem846.exception.DataSourceException;
+
+public interface RasterData {
+
+	public void create(String file) throws DataSourceException;
+	public void dispose() throws DataSourceException;
+	
+	public double getLatitudeResolution();
+	public double getLongitudeResolution();
+	
+	public double getMetersResolution();
+	public double getDataMinimum();
+	public double getDataMaximum();
+	
+	public double getNorth();
+	public double getSouth();
+	public double getEast();
+	public double getWest();
+	
+	public int getRows();
+	public int getColumns();
+	
+	public void calculatenMinAndMax() throws DataSourceException;
+	
+	public double getData(double latitude, double longitude) throws DataSourceException;
+	public double getData(int row, int column) throws DataSourceException;
+	public boolean fillBuffer(double north, double south, double east, double west) throws DataSourceException;
+	public void clearBuffer() throws DataSourceException;
+	
+}
