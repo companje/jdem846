cdfc56c8b626d90d104b0aacfa3bba65d4ad35e7
kmsmgill@gmail.com <kmsmgill@gmail.com@966e72df-b6db-f143-e4ac-e5bb048938f7>
2011-11-16 20:22:32 +0000

    * Some refactoring of code in the shape renderer * Updated shapefile resource loading to use the new resource loader

--- trunk/jdem846/src/us/wthr/jdem846/render/shapelayer/ShapeLayerRenderer.java
+++ trunk/jdem846/src/us/wthr/jdem846/render/shapelayer/ShapeLayerRenderer.java
@@ -26,6 +26,7 @@ import us.wthr.jdem846.shapefile.ShapeConstants;
 import us.wthr.jdem846.shapefile.ShapeFileRequest;
 import us.wthr.jdem846.shapefile.ShapeLayer;
 import us.wthr.jdem846.shapefile.ShapePath;
+import us.wthr.jdem846.shapefile.exception.ShapeFileException;
 import us.wthr.jdem846.shapefile.modeling.FeatureTypeStroke;
 import us.wthr.jdem846.shapefile.modeling.LineStroke;
 
@@ -53,7 +54,7 @@ public class ShapeLayerRenderer
 		this.tileCompletionListeners = tileCompletionListeners;
 	}
 	
-	public void renderShapeLayer() throws RenderEngineException
+	public void render() throws RenderEngineException
 	{
 		if (isCancelled()) {
 			log.warn("Render process cancelled, model not complete.");
@@ -63,7 +64,6 @@ public class ShapeLayerRenderer
 		
 		this.modelCanvas = modelContext.getModelCanvas();
 		
-		//int numLayers = getDataPackage().getShapeFiles().size();
 		int numLayers = getShapeDataContext().getShapeDataListSize();
 		int layerNumber = 0;
 		
@@ -71,48 +71,22 @@ public class ShapeLayerRenderer
 			layerNumber++;
 			try {
 				log.info("Loading shapefile from " + shapeFilePath.getPath());
-				//ShapeBase shapeBase = new ShapeBase(shapeFilePath.getPath(), shapeFilePath.getShapeDataDefinitionId());
+				
 				ShapeBase shapeBase = shapeFilePath.open();
-				ShapeLayer shapeLayer = new ShapeLayer(shapeBase.getShapeType());
-				
-				log.info("Loading " + shapeBase.getShapeCount() + " shapes");
-				for (int i = 0; i < shapeBase.getShapeCount(); i++) {
-					Shape shape = shapeBase.getShape(i);
-					shapeLayer.addShape(shape);
-				}
-				
-				log.info("Translating coordinates...");
-				final MapPoint mapPoint = new MapPoint();
-				shapeLayer.translate(new PointTranslateHandler() {
-					public void translatePoint(double[] coords)
-					{
-						modelCanvas.getMapProjection().getPoint(coords[1], coords[0], 0, mapPoint);
-						coords[0] = mapPoint.column;
-						coords[1] = mapPoint.row;
-						//double x = getRasterDataContext().longitudeToColumn((float) coords[0]);
-						//double y = getRasterDataContext().latitudeToRow((float) coords[1]);
-						//coords[0] = x;
-						//coords[1] = y;
-					}
-				}, false);
-				
-				log.info("Combining shapes...");
-				
-				shapeLayer = shapeLayer.getCombinedPathsByTypes();
-				renderLayer(shapeLayer);
-				//Image layerImage = renderLayer(shapeLayer);
-				//layerImage = layerImage.getScaledInstance(canvas.getWidth(), canvas.getHeight(), Image.SCALE_SMOOTH);
-				//canvas.overlay(layerImage, 0, 0);
-				
+				ShapeLayer shapeLayer = buildShapeLayer(shapeBase);
 				shapeBase.close();
-				// TODO: Restore proper canvas type
+				
+				
+				renderLayer(shapeLayer);
+
+				
+				
 				fireTileCompletionListeners(modelCanvas, ((double)layerNumber) / ((double)numLayers));
 				
 			} catch (OutOfMemoryError err) {
 				throw err;
 			} catch(Exception ex) {
 				throw new RenderEngineException("Error occured rendering shape files", ex);
-				//ex.printStackTrace();
 			}
 			
 			if (isCancelled()) {
@@ -125,71 +99,83 @@ public class ShapeLayerRenderer
 		
 	}
 
+	public ShapeLayer buildShapeLayer(ShapeBase shapeBase) throws RenderEngineException, ShapeFileException
+	{
+		ShapeLayer shapeLayer = new ShapeLayer(shapeBase.getShapeType());
+		
+		log.info("Loading " + shapeBase.getShapeCount() + " shapes");
+		for (int i = 0; i < shapeBase.getShapeCount(); i++) {
+			Shape shape = shapeBase.getShape(i);
+			shapeLayer.addShape(shape);
+		}
+		
+		log.info("Translating coordinates...");
+		final MapPoint mapPoint = new MapPoint();
+		shapeLayer.translate(new PointTranslateHandler() {
+			public void translatePoint(double[] coords)
+			{
+				modelCanvas.getMapProjection().getPoint(coords[1], coords[0], 0, mapPoint);
+				coords[0] = mapPoint.column;
+				coords[1] = mapPoint.row;
+			}
+		}, false);
+		
+		log.info("Combining shapes...");
+		
+		shapeLayer = shapeLayer.getCombinedPathsByTypes();
+		return shapeLayer;
+	}
+	
 	public void renderLayer(ShapeLayer shapeLayer) throws RenderEngineException
 	{
-		//BufferedImage image = new BufferedImage((int)getRasterDataContext().getDataColumns(), (int)getRasterDataContext().getDataRows(), BufferedImage.TYPE_INT_ARGB);
-		
-		//Graphics2D g2d = (Graphics2D) image.getGraphics();
-		//g2d.setColor(new Color(0, 0, 0, 0));
-		//g2d.fillRect(0, 0, image.getWidth(), image.getHeight());
-		//g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
 		int shapeType = shapeLayer.getType();
 		log.info("Shape Type: " + shapeType);
-		fillShapes(shapeLayer);
-
-		//g2d.dispose();
-		//return image;
-
-	}
-
-	protected void fillShapes(ShapeLayer shapeLayer) throws RenderEngineException
-	{
-		log.info("Creating shape paths");
-
-		log.info("Drawing " + shapeLayer.size() + " polylines");
-		int shapeType = shapeLayer.getType();
 
 		for (ShapePath path : shapeLayer.getShapePaths()) {		
-			
-			FeatureTypeStroke featureStroke = null;
-			
-			if (path.getFeatureType() != null) {
-				featureStroke = path.getFeatureType().getFeatureTypeStroke();
-			}
-			
-			if (featureStroke == null) {
-				featureStroke = FeatureTypeStroke.getDefaultFeatureTypeStroke();
-			}
-			
-			List<LineStroke> lineStrokes = featureStroke.getLineStrokes();
-
-			for (LineStroke lineStroke : lineStrokes) {
-			
-				//g2d.setStroke(lineStroke);
-				//g2d.setColor(lineStroke.getColor());
-				
-				if (shapeType == ShapeConstants.TYPE_POLYGON ||
-						shapeType == ShapeConstants.TYPE_POLYGONM ||
-						shapeType == ShapeConstants.TYPE_POLYGONZ) {
-					
-					modelCanvas.fillShape(lineStroke.getColor(), lineStroke, path);
-					//g2d.fill(path);
-					
-				} else if (shapeType == ShapeConstants.TYPE_POLYLINE ||
-							shapeType == ShapeConstants.TYPE_POLYLINEM ||
-							shapeType == ShapeConstants.TYPE_POLYLINEZ) {
-					modelCanvas.drawShape(lineStroke.getColor(), lineStroke, path);
-					//g2d.draw(path);
-
-				}					
-			}	
+			renderPath(shapeType, path);
 			if (isCancelled()) {
 				log.warn("Render process cancelled, model not complete.");
 				break;
 			}
 		}
+	}
+	
+	
+	
 
+	protected void renderPath(int shapeType, ShapePath path) throws RenderEngineException
+	{
+
+		FeatureTypeStroke featureStroke = null;
+			
+		if (path.getFeatureType() != null) {
+			featureStroke = path.getFeatureType().getFeatureTypeStroke();
+		}
+			
+		if (featureStroke == null) {
+			featureStroke = FeatureTypeStroke.getDefaultFeatureTypeStroke();
+		}
+			
+		List<LineStroke> lineStrokes = featureStroke.getLineStrokes();
+
+		for (LineStroke lineStroke : lineStrokes) {
+			
+
+			if (shapeType == ShapeConstants.TYPE_POLYGON ||
+				shapeType == ShapeConstants.TYPE_POLYGONM ||
+				shapeType == ShapeConstants.TYPE_POLYGONZ) {
+					
+				modelCanvas.fillShape(lineStroke.getColor(), lineStroke, path);
+
+			} else if (shapeType == ShapeConstants.TYPE_POLYLINE ||
+						shapeType == ShapeConstants.TYPE_POLYLINEM ||
+						shapeType == ShapeConstants.TYPE_POLYLINEZ) {
+				modelCanvas.drawShape(lineStroke.getColor(), lineStroke, path);
+
+			} else {
+				throw new RenderEngineException("Unsupported shape type: " + shapeType);
+			}
+		}	
 	}
 	
 	protected RasterDataContext getRasterDataContext()
@@ -249,7 +235,7 @@ public class ShapeLayerRenderer
 	public static void render(ModelContext modelContext, ModelCanvas modelCanvas, List<TileCompletionListener> tileCompletionListeners) throws RenderEngineException
 	{
 		ShapeLayerRenderer renderer = new ShapeLayerRenderer(modelContext, modelCanvas, tileCompletionListeners);
-		renderer.renderShapeLayer();
+		renderer.render();
 	}
 	
 	
