d22cee808fc57102493ec94883d9ca3ef22fabf2
kmsmgill@gmail.com <kmsmgill@gmail.com@966e72df-b6db-f143-e4ac-e5bb048938f7>
2011-11-28 20:11:12 +0000

    * Adding more geo loc stuff

--- trunk/jdem846/src-base/us/wthr/jdem846/geo/Spheriod.java
+++ trunk/jdem846/src-base/us/wthr/jdem846/geo/Spheriod.java
@@ -2,5 +2,32 @@ package us.wthr.jdem846.geo;
 
 public class Spheriod
 {
+	private String name;
+	private double semiMajorAxis = 0;
+	private double inverseFlattening = 0;
+	
+	public Spheriod(String name, double semiMajorAxis, double inverseFlattening)
+	{
+		this.name = name;
+		this.semiMajorAxis = semiMajorAxis;
+		this.inverseFlattening = inverseFlattening;
+	}
 
+	protected String getName()
+	{
+		return name;
+	}
+
+	protected double getSemiMajorAxis()
+	{
+		return semiMajorAxis;
+	}
+
+	protected double getInverseFlattening()
+	{
+		return inverseFlattening;
+	}
+	
+	
+	
 }
---
+++ trunk/jdem846/src-base/us/wthr/jdem846/geo/Spheriods.java
@@ -0,0 +1,61 @@
+package us.wthr.jdem846.geo;
+
+public class Spheriods
+{
+	
+	public static final Spheriod AIRY_1830 = new Spheriod("Airy 1830", 6377563.396,	299.3249646);
+	public static final Spheriod AIRY_MODIFIED_1849 = new Spheriod("Airy Modified 1849", 6377340.189,	299.3249646);
+	public static final Spheriod AVERAGE_TERRESTRIAL_SYSTEM_1977 = new Spheriod("Average Terrestrial System 1977", 6378135,	298.257);
+	public static final Spheriod AUSTRALIAN_NATIONAL_SPHERIOD = new Spheriod("Australian National Spheroid", 6378160,	298.25);
+	public static final Spheriod BESSEL_1841 = new Spheriod("Bessel 1841", 6377397.155,	299.1528128);
+	public static final Spheriod BESSEL_MODIFIED = new Spheriod("Bessel Modified", 6377492.018,	299.1528128);
+	public static final Spheriod BESSEL_NAMIBIA = new Spheriod("Bessel Namibia", 6377483.865,	299.1528128);
+	public static final Spheriod CLARKE_1858 = new Spheriod("Clarke 1858", 6378293.64,	294.260676);
+	public static final Spheriod CLARKE_1866 = new Spheriod("Clarke 1866", 6378206.4,	294.9786982);
+	public static final Spheriod CLARKE_1866_MICHIGAN = new Spheriod("Clarke 1866 (Michigan)", 6378450.05,	294.9786847);
+	public static final Spheriod CLARKE_1880 = new Spheriod("Clarke 1880", 6378249.14,	293.466308);
+	public static final Spheriod CLARKE_1880_ARC = new Spheriod("Clarke 1880 (Arc)", 6378249.145,	293.4663077);
+	public static final Spheriod CLARKE_1880_BENOIT = new Spheriod("Clarke 1880 (Benoit)", 6378300.79,	293.4662346);
+	public static final Spheriod CLARKE_1880_IGN = new Spheriod("Clarke 1880 (IGN)", 6378249.2,	293.46602);
+	public static final Spheriod CLARKE_1880_RGS = new Spheriod("Clarke 1880 (RGS)", 6378249.145,	293.465);
+	public static final Spheriod CLARKE_1880_SGA_1922 = new Spheriod("Clarke 1880 (SGA 1922)", 6378249.2,	293.46598);
+	public static final Spheriod EVEREST_1830 = new Spheriod("Everest (1830 Definition)", 6377299.36,	300.8017);
+	public static final Spheriod EVEREST_1830_MODIFIED = new Spheriod("Everest 1830 Modified", 6377304.063,	300.8017);
+	public static final Spheriod EVEREST_ADJUSTMENT_1937 = new Spheriod("Everest Adjustment 1937", 6377276.345,	300.8017);
+	public static final Spheriod EVEREST_1830_1962_DEFINITION = new Spheriod("Everest 1830 (1962 Definition)", 6377301.243,	300.801726);
+	public static final Spheriod EVEREST_1830_1967_DEFINITION = new Spheriod("Everest 1830 (1967 Definition)", 637298.556,	300.8017);
+	public static final Spheriod EVEREST_1830_1975_DEFINITION = new Spheriod("Everest 1830 (1975 Definition)", 6377299.15,	300.801726);
+	public static final Spheriod EVEREST_1969_MODIFIED = new Spheriod("Everest 1969 Modified", 6377295.66,	300.8017);
+	public static final Spheriod FISCHER_1960 = new Spheriod("Fischer 1960", 6378166,	298.3);
+	public static final Spheriod FISCHER_1968 = new Spheriod("Fischer 1968", 6378150.0,	298.3);
+	public static final Spheriod MODIFIED_FISCHER = new Spheriod("Modified Fischer", 6378155.0,	298.3);
+	public static final Spheriod GEM_10C = new Spheriod("GEM 10C", 6378137,	298.2572221);
+	public static final Spheriod GRS_1967 = new Spheriod("GRS 1967", 6378160,	298.247167);
+	public static final Spheriod GRS_1967_TRUNCATED = new Spheriod("GRS 1967 Truncated", 6378160,	298.25);
+	public static final Spheriod GRS_1980 = new Spheriod("GRS 1980", 6378137,	298.2572221);
+	public static final Spheriod HELMERT_1906 = new Spheriod("Helmert 1906", 6378200,	298.3);
+	public static final Spheriod HOUGH_1960 = new Spheriod("Hough 1960", 6378270,	297);
+	public static final Spheriod INDONESIAN_NATIONAL_SPHERIOD = new Spheriod("Indonesian National Spheroid", 6378160,	298.247);
+	public static final Spheriod INTERNATIONAL_1924 = new Spheriod("International 1924", 6378388,	297);
+	public static final Spheriod INTERNATIONAL_1967 = new Spheriod("International 1967", 6378160,	298.25);
+	public static final Spheriod KRASSOWSKY_1940 = new Spheriod("Krassowsky 1940", 7378245,	298.3);
+	public static final Spheriod NWL_9D = new Spheriod("NWL 9D", 6378145,	298.25);
+	public static final Spheriod NWL_10D = new Spheriod("NWL 10D", 6378135,	298.26);
+	public static final Spheriod OSU_86F = new Spheriod("OSU 86F", 6378136.2,	298.25722);
+	public static final Spheriod OSU_91A = new Spheriod("OSU 91A", 6378136.3,	298.25722);
+	public static final Spheriod PLESSIS_1817 = new Spheriod("Plessis 1817", 6376523,	308.64);
+	public static final Spheriod SPHERE = new Spheriod("Sphere", 6371000,	0);
+	public static final Spheriod SPHERE_ARCINFO = new Spheriod("Sphere (ArcInfo)", 6370997,	0);
+	public static final Spheriod STRUVE_1860 = new Spheriod("Struve 1860", 6378298.3,	294.73);
+	public static final Spheriod WALBECK = new Spheriod("Walbeck", 6376896,	302.78);
+	public static final Spheriod WAR_OFFICE = new Spheriod("War Office", 6378300,	296);
+	public static final Spheriod WGS_1966 = new Spheriod("WGS 1966", 6378145,	298.25);
+	public static final Spheriod WGS_1972 = new Spheriod("WGS 1972", 6378135,	298.26);
+	public static final Spheriod WGS_1984 = new Spheriod("WGS 1984", 6378137, 298.2572236);
+
+	
+	private Spheriods()
+	{
+		
+	}
+}
