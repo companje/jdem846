313dc61ee44496cc39918ae84cb7ea477953e32d
kmsmgill <kmsmgill@gmail.com>
2011-11-29 22:35:48 +0000

    * Deleting

--- jdem846/src/us/wthr/jdem846/render/ProcessInterruptListener.java
+++
@@ -1,8 +0,0 @@
-package us.wthr.jdem846.render;
-
-public interface ProcessInterruptListener
-{
-	public void onProcessCancelled();
-	public void onProcessPaused();
-	public void onProcessResumed();
-}
