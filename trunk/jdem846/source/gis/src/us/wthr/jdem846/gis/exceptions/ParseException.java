7aed890475225179e8cb2691c6a6d73ee8bf15a5
kmsmgill@gmail.com <kmsmgill@gmail.com@966e72df-b6db-f143-e4ac-e5bb048938f7>
2012-02-21 01:49:10 +0000

    * Started some new file parsers (incomplete)

---
+++ trunk/jdem846/source/gis/src/us/wthr/jdem846/gis/exceptions/ParseException.java
@@ -0,0 +1,17 @@
+package us.wthr.jdem846.gis.exceptions;
+
+public class ParseException extends Exception
+{
+	
+	public ParseException(String message)
+	{
+		super(message);
+	}
+	
+	public ParseException(String message, Throwable thrown)
+	{
+		super(message, thrown);
+	}
+	
+	
+}
