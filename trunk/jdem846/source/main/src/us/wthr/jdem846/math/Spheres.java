19f80eab8be53a52fea741ad15c43251ff2777fa
kmsmgill@gmail.com <kmsmgill@gmail.com@966e72df-b6db-f143-e4ac-e5bb048938f7>
2012-03-14 20:54:18 +0000

    * Tweaks & documentation

--- trunk/jdem846/source/main/src/us/wthr/jdem846/math/Spheres.java
+++ trunk/jdem846/source/main/src/us/wthr/jdem846/math/Spheres.java
@@ -2,72 +2,49 @@ package us.wthr.jdem846.math;
 
 
 
-public class Spheres extends MathExt
+public class Spheres
 {
 	
-	public static void getPoint3D(double theta, double phi, double radius, double[] points)
+	private static final double RAD_90 = Math.toRadians(90.0);
+	private static final double RAD_180 = Math.toRadians(180.0);
+	private static final double RAD_270 = Math.toRadians(270.0);
+	
+	
+	public static void getPoint3D(double theta, 
+									double phi, 
+									double radius, 
+									double[] points)
 	{
-		theta = radians(theta);
-		phi = radians(phi);
+		theta = Math.toRadians(theta);
+		phi = Math.toRadians(phi);
 		
-		/*
-		double _y = sqrt(sqr(radius) - sqr(radius * cos(phi)));
-		double r0 = sqrt(sqr(radius) - sqr(_y));
+		double _y = Math.sqrt(Math.pow(radius, 2) - Math.pow(radius * Math.cos(phi), 2));
+        double r0 = Math.sqrt(Math.pow(radius, 2) - Math.pow(_y, 2));
 
-		double _b = r0 * cos(theta );
-        double _z = sqrt(sqr(r0) - sqr(_b));
-        double _x = sqrt(sqr(r0) - sqr(_z));
-        */
-		double _y = sqrt(pow(radius, 2) - pow(radius * cos(phi), 2));
-        double r0 = sqrt(pow(radius, 2) - pow(_y, 2));
-
-        double _b = r0 * cos(theta );
-        double _z = sqrt(pow(r0, 2) - pow(_b, 2));
-        double _x = sqrt(pow(r0, 2) - pow(_z, 2));
+        double _b = r0 * Math.cos(theta );
+        double _z = Math.sqrt(Math.pow(r0, 2) - Math.pow(_b, 2));
+        double _x = Math.sqrt(Math.pow(r0, 2) - Math.pow(_z, 2));
 
 		
-        if (theta <= radians(90.0)) {
+        if (theta <= RAD_90) {
             _z *= -1.0;
-	    } else if (theta  <= radians(180.0)) {
+	    } else if (theta  <= RAD_180) {
 	            _x *= -1.0;
 	            _z *= -1.0;
-	    } else if (theta  <= radians(270.0)) {
+	    } else if (theta  <= RAD_270) {
 	            _x *= -1.0;
 	    }
 	
 	    if (phi >= 0) { 
-	            _y = abs(_y);
+	            _y = Math.abs(_y);
 	    } else {
-	            _y = abs(_y) * -1;
+	            _y = Math.abs(_y) * -1;
 	    }
-
-        /*
-        if (theta <= 180.0) {
-        	_z *= -1.0;
-        }
-        
-        if (theta > 90.0 && theta <= 270.0) {
-        	_x *= -1.0;
-        }
-
-        if (phi >= 0) { 
-                _y = abs(_y);
-        } else {
-                _y = abs(_y) * -1;
-        }
-		*/
 
         points[0] = _x;
         points[1] = _y;
         points[2] = _z;
 
-       // double mag = sqrt(sqr(_x)+sqr(_y)+sqr(_z));   
-       // _x /= mag;   
-        //_y /= mag;   
-       // _z /= mag; 
-        
-        //points[3] = (atan2(_x, _z)/(Math.PI*2)) + 0.5f;   
-       // points[4] =  (asin(_y) / Math.PI) + 0.5f;
 	}
 	
 }
