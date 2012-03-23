81dde5f26f3532484688135782dac265f88be764
kmsmgill@gmail.com <kmsmgill@gmail.com@966e72df-b6db-f143-e4ac-e5bb048938f7>
2012-03-23 19:47:43 +0000

    * Added calculation of mean elevation in min/max calculator * Created simple class to generate gridded min/max/mean values for a dataset. This data will then be supplied to a method of estimating min/max rather than scanning an entire dataset on demand (unless the user opts to)

--- trunk/jdem846/source/main/src/us/wthr/jdem846/render/ElevationMinMax.java
+++ trunk/jdem846/source/main/src/us/wthr/jdem846/render/ElevationMinMax.java
@@ -4,10 +4,20 @@ public class ElevationMinMax
 {
 	public double minimum;
 	public double maximum;
+	public double mean;
+	public double median;
 	
 	public ElevationMinMax(double minimum, double maximum)
 	{
+		this(minimum, maximum, (minimum + maximum) / 2.0, 0.0);
+	}
+	
+	public ElevationMinMax(double minimum, double maximum, double mean, double median)
+	{
 		this.minimum = minimum;
 		this.maximum = maximum;
+		this.mean = mean;
+		this.median = median;
 	}
+	
 }
---
+++ trunk/jdem846/source/main/test/us/wthr/jdem846/ElevationEstimationDatasetTestMain.java
@@ -0,0 +1,117 @@
+package us.wthr.jdem846;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import us.wthr.jdem846.lighting.LightingContext;
+import us.wthr.jdem846.logging.Log;
+import us.wthr.jdem846.logging.Logging;
+import us.wthr.jdem846.rasterdata.RasterData;
+import us.wthr.jdem846.rasterdata.RasterDataContext;
+import us.wthr.jdem846.rasterdata.RasterDataProviderFactory;
+import us.wthr.jdem846.render.ElevationMinMax;
+import us.wthr.jdem846.render.ElevationMinMaxCalculator;
+import us.wthr.jdem846.shapedata.ShapeDataContext;
+
+public class ElevationEstimationDatasetTestMain extends AbstractTestMain
+{
+	private static Log log = null;
+	
+	
+	public static void main(String[] args)
+	{
+		try {
+			initialize(false);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		log = Logging.getLog(ElevationEstimationDatasetTestMain.class);
+		
+		try {
+			ElevationEstimationDatasetTestMain testMain = new ElevationEstimationDatasetTestMain();
+			testMain.doTesting();
+		} catch (Exception ex) {
+			log.error("Uncaught exception while running test main: " + ex.getMessage(), ex);
+		}
+		
+	}
+	
+	
+	
+	public void doTesting() throws Exception
+	{
+		List<String> inputDataList = new LinkedList<String>();
+		
+		inputDataList.add("C:\\srv\\elevation\\GEBCO08\\gebco_08.flt");
+		
+		String saveOutputTo = JDem846Properties.getProperty("us.wthr.jdem846.testOutputPath") + "/elevation-minmax-samples.csv";
+		
+		File csvFile = new File(saveOutputTo);
+		PrintWriter out = new PrintWriter(csvFile);
+		
+		
+		RasterDataContext dataProxy = new RasterDataContext();
+		
+		for (String inputDataPath : inputDataList) {
+			log.info("Adding raster data @ '" + inputDataPath + "'");
+			RasterData rasterData = RasterDataProviderFactory.loadRasterData(inputDataPath);
+			dataProxy.addRasterData(rasterData);
+			
+		}
+		
+		double latitudeResolution = 1.0;
+		double longitudeResolution = 1.0;
+		
+		//dataProxy.setEffectiveLatitudeResolution(latitudeResolution);
+		//dataProxy.setEffectiveLongitudeResolution(longitudeResolution);
+		
+		
+		ShapeDataContext shapeContext = new ShapeDataContext();
+		LightingContext lightingContext = new LightingContext();
+		ModelOptions modelOptions = new ModelOptions();
+		modelOptions.setWidth(dataProxy.getDataColumns());
+		modelOptions.setHeight(dataProxy.getDataRows());
+		
+		
+		ModelContext modelContext = ModelContext.createInstance(dataProxy, shapeContext, lightingContext, modelOptions);
+		modelContext.updateContext();
+		
+		
+		for (double latitude = 90.0; latitude > -90.0; latitude -= latitudeResolution) {
+			
+			for (double longitude = -180.0; longitude < 180; longitude += longitudeResolution) {
+				
+				modelContext.setNorthLimit(latitude);
+				modelContext.setSouthLimit(latitude - latitudeResolution);
+				
+				modelContext.setWestLimit(longitude);
+				modelContext.setEastLimit(longitude + longitudeResolution);
+				
+				ElevationMinMaxCalculator minMaxCalc = new ElevationMinMaxCalculator(modelContext);
+				ElevationMinMax minMax = minMaxCalc.calculateMinAndMax();
+				
+				double min = minMax.minimum;
+				double max = minMax.maximum;
+				double mean = minMax.mean;
+				double median = minMax.median;
+				
+				
+				out.printf("%f, %f, %f, %f, %f, %f\n", latitude, longitude, min, max, mean, median);
+				log.info("" + latitude + ", " + longitude + ", " + min + ", " + max + ", " + mean + ", " + median);
+				
+				//break;
+			}
+			
+			break;
+		}
+
+		
+		
+		out.flush();
+		out.close();
+	}
+	
+}
