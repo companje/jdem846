8f0d4fff63afc9969ec09eb92ecf348e00454fe1
kmsmgill <kmsmgill@gmail.com>
2012-03-28 19:38:43 +0000

    * Clean up

--- jdem846/source/main/src/us/wthr/jdem846/rasterdata/ElevationDataMap.java
+++ jdem846/source/main/src/us/wthr/jdem846/rasterdata/ElevationDataMap.java
@@ -1,13 +1,10 @@
 package us.wthr.jdem846.rasterdata;
 
-import java.util.Hashtable;
 
 import us.wthr.jdem846.DemConstants;
 import us.wthr.jdem846.gis.CoordinateSpaceAdjuster;
-import us.wthr.jdem846.gis.elevation.ElevationSample;
-import us.wthr.jdem846.gis.exceptions.CoordinateSpaceException;
 
-@SuppressWarnings("serial")
+
 public class ElevationDataMap
 {
 	
@@ -26,7 +23,7 @@ public class ElevationDataMap
 	
 	private double[] values;
 	
-	public ElevationDataMap(double north, double south, double east, double west, double latitudeResolution, double longitudeResolution, int initialCapacity, float loadFactor)
+	public ElevationDataMap(double north, double south, double east, double west, double latitudeResolution, double longitudeResolution)
 	{
 		//super(initialCapacity, loadFactor);
 		
@@ -70,6 +67,10 @@ public class ElevationDataMap
 	
 	public double put(double latitude, double longitude, double elevation)
 	{
+		if (values == null) {
+			return elevation;
+		}
+		
 		double adjLatitude = 0;
 		double adjLongitude = 0;
 		
@@ -81,15 +82,7 @@ public class ElevationDataMap
 			return elevation;
 		}
 		
-		/*
-		try {
-			adjLatitude = coordinateSpaceAdjuster.adjustLatitude(latitude);
-			adjLongitude = coordinateSpaceAdjuster.adjustLongitude(longitude);
-		} catch (CoordinateSpaceException ex) {
-			return elevation;
-		}
-		*/
-		
+
 		int index = getIndex(adjLatitude, adjLongitude);
 		if (index < 0 || index >= values.length) {
 			return elevation;
@@ -97,12 +90,15 @@ public class ElevationDataMap
 		
 		values[index] = elevation;
 		return elevation;
-		//int key = getHashKey(adjLatitude, adjLongitude);
-		//return super.put(key, elevation);
+		
 	}
 	
-	public Double get(Double latitude, double longitude, double ifNull)
+	public double get(double latitude, double longitude, double ifNull)
 	{
+		
+		if (values == null) {
+			return ifNull;
+		}
 		
 		double adjLatitude = 0;
 		double adjLongitude = 0;
@@ -125,42 +121,13 @@ public class ElevationDataMap
 			return value;
 		else
 			return ifNull;
-		
-		/*
-		int key = getHashKey(adjLatitude, adjLongitude);
-		Double value = super.get(key);
-		if (value != null) {
-			return value;
-		} else {
-			return ifNull;
-		}
-		*/
-		
+	
 	}
 	
-	
-	protected int getHashKey(double latitude, double longitude)
-	{
-		//int columns = (int) Math.ceil((east - west) / longitudeResolution);
-		
-		
-		
-		int row = (int) Math.round((north - latitude) / latitudeResolution);
-		int column = (int) Math.round((longitude - west) / longitudeResolution);
-		
-		int key = (row * columns) + column;
-		return key;
-	}
+
 	
 	public static ElevationDataMap create(double north, double south, double east, double west, double latitudeResolution, double longitudeResolution)
 	{
-		int rows = (int) Math.ceil((north - south) / latitudeResolution) + 1;
-		int columns = (int) Math.ceil((east - west) / longitudeResolution) + 1;
-		int initialCapacity = rows * columns;
-		float loadFactor = 1.25f;
-		
-		return new ElevationDataMap(north, south, east, west, latitudeResolution, longitudeResolution, initialCapacity, loadFactor);
-		
-		
+		return new ElevationDataMap(north, south, east, west, latitudeResolution, longitudeResolution);
 	}
 }
