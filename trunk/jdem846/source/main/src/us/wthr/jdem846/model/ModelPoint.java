ffba8913ce541dc87f5e4f1812b815fb4ee73470
kmsmgill@gmail.com <kmsmgill@gmail.com@966e72df-b6db-f143-e4ac-e5bb048938f7>
2012-04-09 14:50:14 +0000

    * Moved grid rendering/processing code to new package. Much more to be done...

---
+++ trunk/jdem846/source/main/src/us/wthr/jdem846/model/ModelPoint.java
@@ -0,0 +1,95 @@
+package us.wthr.jdem846.model;
+
+import us.wthr.jdem846.DemConstants;
+import us.wthr.jdem846.render.util.ColorUtil;
+
+public class ModelPoint
+{
+	
+	private double elevation = DemConstants.ELEV_NO_DATA;
+	private double[] normal = new double[3];
+	private double dotProduct;
+	private int rgba;
+	private int shadedRgba;
+	
+	public ModelPoint()
+	{
+		
+	}
+
+	public double getElevation()
+	{
+		return elevation;
+	}
+
+	public void setElevation(double elevation)
+	{
+		this.elevation = elevation;
+	}
+
+	public void getNormal(double[] fill)
+	{
+		fill[0] = normal[0];
+		fill[1] = normal[1];
+		fill[2] = normal[2];
+	}
+	
+	public double[] getNormal()
+	{
+		return normal;
+	}
+
+	public void setNormal(double[] normal)
+	{
+		this.normal[0] = normal[0];
+		this.normal[1] = normal[1];
+		this.normal[2] = normal[2];
+	}
+
+	public double getDotProduct()
+	{
+		return dotProduct;
+	}
+
+	public void setDotProduct(double dotProduct)
+	{
+		this.dotProduct = dotProduct;
+	}
+
+	
+	public void getRgba(int[] fill, boolean shaded) 
+	{
+		ColorUtil.intToRGBA(getRgba(shaded), fill);
+	}
+	
+	public int getRgba(boolean shaded)
+	{
+		if (shaded) {
+			return this.shadedRgba;
+		} else {
+			return rgba;
+		}
+	}
+
+	public void setRgba(int rgba, boolean shaded)
+	{
+		if (shaded) {
+			this.shadedRgba = rgba;
+		} else {
+			this.rgba = rgba;
+		}
+	}
+	
+	public void setRgba(int[] rgba, boolean shaded)
+	{
+		this.setRgba(ColorUtil.rgbaToInt(rgba), shaded);
+	}
+	
+	public void setRgba(int r, int g, int b, int a, boolean shaded)
+	{
+		this.setRgba(ColorUtil.rgbaToInt(r, g, b, a), shaded);
+	}
+	
+	
+	
+}
---
+++ trunk/jdem846/source/main/src/us/wthr/jdem846/model/processing/GridProcessor.java
@@ -0,0 +1,11 @@
+package us.wthr.jdem846.model.processing;
+
+import us.wthr.jdem846.exception.RenderEngineException;
+
+public interface GridProcessor
+{
+	public boolean isProcessing();
+	public void prepare() throws RenderEngineException;
+	public void process() throws RenderEngineException;
+	
+}
