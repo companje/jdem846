7b64a9aa730a87d285252d79dbe1a09505f56db7
kmsmgill <kmsmgill@gmail.com>
2012-11-19 13:23:12 +0000

    * More actions

---
+++ jDem846UI/src/us/wthr/jdem846ui/actions/ExportDataAction.java
@@ -0,0 +1,26 @@
+package us.wthr.jdem846ui.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.ui.IWorkbenchWindow;
+
+import us.wthr.jdem846ui.Activator;
+import us.wthr.jdem846ui.ICommandIds;
+
+public class ExportDataAction extends Action {
+	private final IWorkbenchWindow window;
+	private int instanceNum = 0;
+	private final String viewId;
+	
+	
+	public ExportDataAction(IWorkbenchWindow window, String label, String viewId)
+	{
+		this.window = window;
+		this.viewId = viewId;
+        setText(label);
+        // The id is used to refer to the action in a menu or toolbar
+		setId(ICommandIds.CMD_EXPORT_DATA);
+        // Associate the action with a pre-defined command, to allow key bindings.
+		setActionDefinitionId(ICommandIds.CMD_EXPORT_DATA);
+		setImageDescriptor(Activator.getImageDescriptor("/icons/eclipse/data_export.gif"));
+	}
+}
