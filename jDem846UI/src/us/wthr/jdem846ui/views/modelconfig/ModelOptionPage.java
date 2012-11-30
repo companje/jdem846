0b79f90c9991509e7c7a2890c1228f6a3502cc48
kmsmgill <kmsmgill@gmail.com>
2012-11-30 21:58:34 +0000

    * Added additional model option controls * Other additions

--- jDem846UI/src/us/wthr/jdem846ui/views/modelconfig/ModelOptionPage.java
+++ jDem846UI/src/us/wthr/jdem846ui/views/modelconfig/ModelOptionPage.java
@@ -8,19 +8,10 @@ import org.eclipse.ui.forms.widgets.TableWrapLayout;
 
 import us.wthr.jdem846.logging.Log;
 import us.wthr.jdem846.logging.Logging;
-import us.wthr.jdem846.model.AzimuthElevationAngles;
-import us.wthr.jdem846.model.LightingDate;
-import us.wthr.jdem846.model.LightingTime;
 import us.wthr.jdem846.model.OptionModel;
 import us.wthr.jdem846.model.OptionModelContainer;
 import us.wthr.jdem846.model.OptionModelPropertyContainer;
-import us.wthr.jdem846.model.RgbaColor;
-import us.wthr.jdem846.model.ViewPerspective;
 import us.wthr.jdem846.model.exceptions.InvalidProcessOptionException;
-import us.wthr.jdem846ui.views.modelconfig.controls.BooleanControl;
-import us.wthr.jdem846ui.views.modelconfig.controls.DoubleControl;
-import us.wthr.jdem846ui.views.modelconfig.controls.IntegerControl;
-import us.wthr.jdem846ui.views.modelconfig.controls.ListControl;
 
 
 public class ModelOptionPage extends Composite
@@ -61,58 +52,9 @@ public class ModelOptionPage extends Composite
 		
 		List<OptionModelPropertyContainer> properties = container.getProperties();
 		for (OptionModelPropertyContainer propertyContainer : properties) {
-			addPropertyControl(propertyContainer);
+			ModelOptionControlsFactory.createControl(propertyContainer.getListModelClass(), this, propertyContainer);
 		}
 	}
 	
-	
-
-	protected void addPropertyControl(OptionModelPropertyContainer propertyContainer)
-	{
-		
-		if (!propertyContainer.getListModelClass().equals(Object.class)) {
-			addListPropertyControl(propertyContainer);
-		} else if (propertyContainer.getType().equals(boolean.class)) {
-			addBooleanPropertyControl(propertyContainer);
-		} else if (propertyContainer.getType().equals(RgbaColor.class)) {
-			//addColorPropertyControl(propertyContainer);
-		} else if (propertyContainer.getType().equals(AzimuthElevationAngles.class)) {
-			//addAzimuthElevationAnglesControl(propertyContainer);
-		} else if (propertyContainer.getType().equals(ViewPerspective.class)) {
-			//addViewPerspectiveControl(propertyContainer);
-		} else if (propertyContainer.getType().equals(double.class)) {
-			addDoublePropertyControl(propertyContainer);
-		} else if (propertyContainer.getType().equals(int.class)) {
-			addIntegerPropertyControl(propertyContainer);
-		} else if (propertyContainer.getType().equals(LightingDate.class)) {
-			//addDateControl(propertyContainer);
-		} else if (propertyContainer.getType().equals(LightingTime.class)) {
-			//addTimeControl(propertyContainer);
-		}
-		
-		
-		
-	}
-	
-	
-	protected void addBooleanPropertyControl(OptionModelPropertyContainer propertyContainer)
-	{
-		BooleanControl.create(this, propertyContainer, propertyContainer.getLabel());
-	}
-	
-	protected void addIntegerPropertyControl(OptionModelPropertyContainer propertyContainer)
-	{
-		IntegerControl.create(this, propertyContainer, propertyContainer.getLabel() + ":");
-	}
-	
-	protected void addDoublePropertyControl(OptionModelPropertyContainer propertyContainer)
-	{
-		DoubleControl.create(this, propertyContainer, propertyContainer.getLabel() + ":");
-	}
-	
-	protected void addListPropertyControl(OptionModelPropertyContainer propertyContainer)
-	{
-		ListControl.create(this, propertyContainer, propertyContainer.getLabel() + ":");
-	}
 	
 }
