25c0cf1914b3f685ea89afabe6646f1831f82f15
kmsmgill@gmail.com <kmsmgill@gmail.com@966e72df-b6db-f143-e4ac-e5bb048938f7>
2012-12-06 01:01:33 +0000

    * Fix to model builder as to when it initializes the globalOptionModel variable (was a NullPointerException) * Implemented fillBuffer for GenericRasterDataProvider

--- trunk/jdem846/source/main/src/us/wthr/jdem846/rasterdata/generic/GenericRasterDataProvider.java
+++ trunk/jdem846/source/main/src/us/wthr/jdem846/rasterdata/generic/GenericRasterDataProvider.java
@@ -8,6 +8,7 @@ import us.wthr.jdem846.logging.Log;
 import us.wthr.jdem846.logging.Logging;
 import us.wthr.jdem846.rasterdata.AbstractRasterDataProvider;
 import us.wthr.jdem846.rasterdata.RasterData;
+import us.wthr.jdem846.rasterdata.RasterDataLatLongBox;
 
 public class GenericRasterDataProvider extends AbstractRasterDataProvider 
 {
@@ -108,11 +109,61 @@ public class GenericRasterDataProvider extends AbstractRasterDataProvider
 	}
 
 	@Override
-	public boolean fillBuffer(double north, double south, double east,
-			double west) throws DataSourceException 
+	public boolean fillBuffer(double north, double south, double east, double west) throws DataSourceException 
 	{
-		// TODO Auto-generated method stub
-		return false;
+		RasterDataLatLongBox bufferBox = new RasterDataLatLongBox(north, south, east, west);
+		if (!this.intersects(bufferBox)) {
+			return false;
+		}
+		
+		// Adjust the range to fit what this data supports
+		
+
+		// TODO: Too simplistic
+		if (north > this.getNorth())
+			north = this.getNorth();
+		if (south < this.getSouth())
+			south = this.getSouth();
+		if (east > this.getEast())
+			east = this.getEast();
+		if (west < this.getWest())
+			west = this.getWest();
+		
+		// TODO: Too simplistic
+		int x = (int) Math.floor(this.longitudeToColumn(west));
+		int y = (int) Math.floor(this.latitudeToRow(north));
+
+		int x2 = (int) Math.ceil(this.longitudeToColumn(east));
+		int y2 = (int) Math.ceil(this.latitudeToRow(south));
+		
+		// We add 2 columns & 2 rows to support data point interpolation (done in AbstractRasterDataProvider)
+		int columns = x2 - x + 2;
+		int rows = y2 - y + 2;
+
+		if (columns <= 0 || rows <= 0) {
+			return false;
+		}
+		
+		if (x + columns > rasterDefinition.getImageWidth()) {
+			columns = rasterDefinition.getImageWidth() - x;
+		}
+		
+		if (y + rows > rasterDefinition.getImageHeight()) {
+			rows = rasterDefinition.getImageHeight() - y;
+		}
+		
+		log.info("Filling raster buffer...");
+		boolean status;
+		try {
+			if ((status = dataReader.fillBuffer(x, y, columns, rows))) {
+				log.info("Filled raster buffer");
+			} else {
+				log.info("Raster buffer not filled.");
+			}
+		} catch (Exception ex) {
+			throw new DataSourceException("Error attempting to cache more data than actually exists: " + ex.getMessage(), ex);
+		}
+		return status;
 	}
 
 	@Override
