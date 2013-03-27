5aa3a0edc8fc5a013f2a065d3e3a81fd4ed62954
kmsmgill <kmsmgill@gmail.com>
2013-03-27 01:41:55 +0000

    * Fix to JavaScript compiler to ensure correct class loader. This is a defensive fix when the core library is included as an Eclipse plugin which screws with the classpath

--- jdem846/source/main/src/us/wthr/jdem846/scripting/javascript/JavaScriptLoader.java
+++ jdem846/source/main/src/us/wthr/jdem846/scripting/javascript/JavaScriptLoader.java
@@ -1,9 +1,11 @@
 package us.wthr.jdem846.scripting.javascript;
 
+
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ImporterTopLevel;
 import org.mozilla.javascript.Scriptable;
 
+import us.wthr.jdem846.Activator;
 import us.wthr.jdem846.exception.ScriptCompilationFailedException;
 import us.wthr.jdem846.exception.ScriptingException;
 import us.wthr.jdem846.scripting.ScriptProxy;
@@ -14,6 +16,7 @@ public class JavaScriptLoader
 	{
 		
 		Context cx = Context.enter();
+		cx.setApplicationClassLoader(Activator.class.getClassLoader());
 		Scriptable scope = new ImporterTopLevel(cx);
 		Object result = cx.evaluateString(scope, scriptContent, "<cmd>", 1, null);
 		return new JavaScriptProxy(scope);
